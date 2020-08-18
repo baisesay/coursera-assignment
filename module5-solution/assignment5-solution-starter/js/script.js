f62f58b3e57e4ad3fc6aea23ff1bddaeb03586ef
baisesay <65818785+baisesay@users.noreply.github.com>
2020-08-18 10:52:13 +0000

    Update script.js

--- module5-solution/assignment5-solution-starter/js/script.js
+++ module5-solution/assignment5-solution-starter/js/script.js
@@ -82,19 +82,9 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
         // On first load, show home view
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(
-            homeHtmlUrl,
-            function(responseText) {
-                document.querySelector("#main-content").innerHTML = responseText;
-            },
-            false);
-
-        dc.loadMenuCategories = function() {
-            showLoading("#main-content");
-            $ajaxUtils.sendGetRequest(
-                allCategoriesUrl, buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-                true); // Explicitly setting the flag to get JSON from server processed into an object literal
-        }
-
+            allCategoriesUrl,
+            buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+            true); // Explicitly setting the flag to get JSON from server processed into an object literal
     });
     // *** finish **
 
@@ -108,25 +98,10 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
             homeHtmlUrl,
             function(homeHtml) {
 
-                $ajaxUtils.sendGetRequest(
-                    categoriesTitleHtml,
-                    function(categoriesTitleHtml) {
-
-                        $ajaxUtils.sendGetRequest(
-                            categoryHtml,
-                            function(categoryHtml) {
-                                chooseRandomCategory;
-                                var chosenCategoryShortName = buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml);
-                                insertHtml("#main-content", chosenCategoryShortName);
-                            },
-
-                            false);
-                        // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
-                        // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
-                        // variable's name implies it expects.
-                        // var chosenCategoryShortName = ....
-                    },
-                    false);
+                // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
+                // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
+                // variable's name implies it expects.
+                var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
                 // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
                 // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -139,17 +114,20 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // Hint: you need to surround the chosen category short name with something before inserting
                 // it into the home html snippet.
                 //
-                // var homeHtmlToInsertIntoMainPage = ....
+
+                chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
+                var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
                 // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
                 // of how to do that.
                 // ....
+                insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
             },
             false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
-    };
+    }
 
 
     // Given array of category objects, returns a random category object.
@@ -159,7 +137,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
 
         // return category object with that randomArrayIndex
         return categories[randomArrayIndex];
-    };
+    }
 
 
     // Load the menu categories view
@@ -177,7 +155,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(
             menuItemsUrl + categoryShort,
-            buildAndShowMenuItemsHTML, buildAndShowHomeHTML);
+            buildAndShowMenuItemsHTML);
     };
 
 
